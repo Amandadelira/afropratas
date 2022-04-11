18bc7b0022ce606cfd378cd82a7fd324521aa4a7
Amandadelira <amanda.lira@aluna.ifsp.edu.br>
2022-04-11 06:08:38 -0300

    Aula de 11/04 api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ApiRequest from "./ApiRequest";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> 
-          Alterei no vscode!
-           </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <h1>Clean</h1>
+    <ApiRequest />
+  </>
   );
 }
-
 export default App;

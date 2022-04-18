ad691d29b2c1c96c7b038e2cb8dc88d1428b8661
Amandadelira <amanda.lira@aluna.ifsp.edu.br>
2022-04-18 06:36:12 -0300

    Aula de 18/04!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import ApiRequest from "./ApiRequest";
-<<<<<<< HEAD
-
 
 function App() {
   return(
-    <>
-    <h1>Clean</h1>
-    <ApiRequest />
-    </>
-=======
-
-function App() {
-  return (
-  <>
-    <h1>Clean</h1>
-    <ApiRequest />
-  </>
->>>>>>> 7046c3dba3c9dfc5ca5e21336f1e033d5c3c3978
+    <div className="App">
+      <h1>Site Afropratas</h1>
+    </div>
   );
 }
 

81c01360a9dfa555bc1f6efd068eea89cb408927
Amandadelira <amanda.lira@aluna.ifsp.edu.br>
2022-04-25 06:24:09 -0300

    Aula de 25/04

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import './App.css';
+import AddImage from './estudo/addimage';
+import ExemploContexto from './estudo/ExemploContexto';
+import UsandoProps from './estudo/UsandoProps'
+import LangContext from './estudo/LangContext'
+import { useState } from 'react'
+import UsandoChildren from './estudo/UsandoChildren';
+import UsandoIcons from './estudo/UsandoIcons';
 
 function App() {
-  return(
+
+  const lang = {
+    br: {
+      welcome: 'Bem-Vindo',
+      changeLanguage: 'Alterar Idioma',
+      portuguese: 'Português',
+      english: 'Inglês',
+      textIntroduction: ''
+    },
+    en: {
+      welcome: 'Welcome',
+      changeLanguage: 'Change Language',
+      portuguese: 'Portuguese',
+      english: 'English'
+    }
+  }
+
+ const currentLangState = useState('br');
+ const [currentLang, setCurrentLanguage] = currentLangState;
+
+ const toggleLanguage = () => {
+  const newLanguage = currentLang === 'br' ? 'en' : 'br'
+  setCurrentLanguage(newLanguage)
+ }
+
+ return (
+  <LangContext.Provider value={[
+      currentLang === 'br' ? lang.br : lang.en,
+      toggleLanguage,
+      setCurrentLanguage
+    ]}>
     <div className="App">
-      <h1>Site Afropratas</h1>
+      <UsandoProps />
+      <AddImage nome="Renan"/>
+      <ExemploContexto/>
+      <UsandoChildren value={10}>
+        <h1>Usei o Children</h1>
+      </UsandoChildren>
+      <UsandoChildren value={5} >
+        <h2>Usei o Children 2</h2>
+        <p>Bla bla bla</p>
+        <UsandoIcons />
+      </UsandoChildren>
     </div>
+  </LangContext.Provider>
   );
 }
 
